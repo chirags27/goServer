488a5f9c0fb9efb6079bfb1ea6de6e903a2d7fbc
chirags <chiragshah271093@gmail.com>
2016-07-19 22:49:23 -0400

    Adding formatting

--- src/client.go
+++ src/client.go
@@ -1,53 +1,44 @@
 package main
 
-import(
-"fmt"
-"net"
-"os"
-"encoding/json"
-// "time"
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+	// "time"
 )
 
 var tcpAddr string = "127.0.0.1:4560"
 
 var targetDirectory string = "downloaded"
 
-
-type  FileRequest struct{
-
+type FileRequest struct {
 	Username string
 	Password string
-	File 	 string
+	File     string
 }
 
-type  FileResponse struct{
-
-	Status 			 int
-	FileContents 	 string
+type FileResponse struct {
+	Status       int
+	FileContents string
 }
 
-
-
-
-func main(){
+func main() {
 
 	// Get the credentails and files requested
-	
-	if len(os.Args) !=4 && len(os.Args) !=5{
+
+	if len(os.Args) != 4 && len(os.Args) != 5 {
 		fmt.Printf("USAGE: ./client <ID> <PASSWORD> <FILE> <TARGET_DIRECTORY>\n")
 		fmt.Printf("Default Target Direcory: downloaded/ \n")
 		return
 	}
-	if len(os.Args) == 5{
+	if len(os.Args) == 5 {
 		targetDirectory = os.Args[4]
 	}
 
-
-
-
 	conn, err := net.Dial("tcp", tcpAddr)
 
-	if err!=nil{
+	if err != nil {
 		fmt.Printf("Error while connecting to the server.\n")
 		return
 	}
@@ -63,16 +54,14 @@ func main(){
 	fmt.Printf("File Requested: %s\n", reqToSend.File)
 
 	jsonReq, err := json.Marshal(&reqToSend)
-	if err!=nil{
+	if err != nil {
 		fmt.Printf("Error in encoding struct to json")
 		return
 	}
 
+	_, err = conn.Write(jsonReq)
 
-
-	_,err = conn.Write(jsonReq)
-
-	if err!=nil{
+	if err != nil {
 		fmt.Printf("Error in sending json message to server")
 		return
 	}
@@ -82,12 +71,12 @@ func main(){
 	d := json.NewDecoder(conn)
 	var resp FileResponse
 	err = d.Decode(&resp)
-	if err!=nil{
+	if err != nil {
 		fmt.Printf("Error in decoding the received message.\n")
 		return
 	}
 
-	if resp.Status != 1{
+	if resp.Status != 1 {
 		fmt.Printf("Invalid ID, Password or Filename. Try again.\n")
 		return
 	}
@@ -95,31 +84,30 @@ func main(){
 	printChannel := make(chan int)
 	go printRoutine(printChannel)
 
-	printChannel<-1
+	printChannel <- 1
 	// Create a file in the local directory and write the contents to it
 	err = os.Mkdir(targetDirectory, 0777)
-	f,err := os.Create(targetDirectory + "/" + reqToSend.File)
-	err = os.Chmod(targetDirectory + "/" + reqToSend.File, 0666)
+	f, err := os.Create(targetDirectory + "/" + reqToSend.File)
+	err = os.Chmod(targetDirectory+"/"+reqToSend.File, 0666)
 
-	if err!=nil{
+	if err != nil {
 		panic(err)
 	}
 
 	_, err = f.Write([]byte(resp.FileContents))
 
-	if err!=nil{
+	if err != nil {
 		panic(err)
 	}
-	printChannel<-1
+	printChannel <- 1
 	<-printChannel
 }
 
-
-func printRoutine(printChannel chan int){
+func printRoutine(printChannel chan int) {
 	<-printChannel
 	fmt.Printf("Started writing the received contents!\n")
 	<-printChannel
 	fmt.Printf("Done writing the received contents!\n")
-	printChannel<-1
+	printChannel <- 1
 
 }
